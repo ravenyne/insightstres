37fe31ec604ea983661568a0520d65070280478f
ravenyne <elliestar444wr@gmail.com>
2026-02-21 21:27:59 +0800

    feat(notifications): Create password reset notification

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+    public $email;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token, $email)
+    {
+        $this->token = $token;
+        $this->email = $email;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $resetUrl = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $this->email,
+        ], false));
+
+        return (new MailMessage)
+                    ->subject('Reset Password - Insight Stress')
+                    ->view('emails.reset-password', ['resetUrl' => $resetUrl]);
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
